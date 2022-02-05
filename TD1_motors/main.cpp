59ec6fe2e80c82dd24e2b67a33a6a8fb716f380a
Karol Wojslaw <wojslaw.tech@gmail.com>
2022-02-05 16:01:42 +0100

    Add Pwm class

--- TD1_motors/main.cpp
+++ TD1_motors/main.cpp
@@ -9,6 +9,27 @@
 C12832 lcd(D11, D13, D12, D7, D10); // LCD Initialisation (pin assignment)
 
 /* -------------------------- Main function --------------------------- */
+/* ----------------------- Pwm class ----------------------- */
+class Pwm {
+
+private:
+    PwmOut _pwm;        // Create PwmOut object
+    int _frequency;     // Frequency to be set
+    int _period_us_val; // Period to be set (= 1/_frequency)
+
+public:
+    Pwm(PinName pin, int frequency) : _pwm(pin), _frequency(frequency) {
+        _period_us_val = (1000000 / frequency);  // Calculate the period value [micro seconds]
+        _pwm.period_us(_period_us_val);           // Set the period of the pwm otuput
+    }
+
+    void set(float duty_cycle) {
+        _pwm.write(duty_cycle);  // Set the duty cycle (val between 0.0 - 1.0)
+    }
+};
+
+
+/* ----------------------- Main function ----------------------- */
 int main() {
     
     while(1) {  // Main while loop of the program

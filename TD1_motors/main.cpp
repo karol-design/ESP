6cae08be8fa9f212740d9d627e63595952cfe0ac
Karol Wojslaw <wojslaw.tech@gmail.com>
2022-02-05 02:35:47 +0100

    Create main.cpp for TD1

---
+++ TD1_motors/main.cpp
@@ -0,0 +1,17 @@
+/* Technical Demonstration 1 - Motors
+ * Description: This code is intended to run on the STM32 MCU
+ * Date: 05/02/2022
+ */
+
+#include "mbed.h"   // Import the Mbed libraries
+#include "C12832.h" // Import the C12832 LCD screen llibrary
+
+C12832 lcd(D11, D13, D12, D7, D10); // LCD Initialisation (pin assignment)
+
+/* -------------------------- Main function --------------------------- */
+int main() {
+    
+    while(1) {  // Main while loop of the program
+
+    }
+}
